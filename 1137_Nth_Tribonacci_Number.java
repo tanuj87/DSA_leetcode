ae15dda6d6fec293e6bdcff53b8e57608b12359c
Tanuj <tanuj.mach10@gmail.com>
2024-09-18 16:11:02 +0530

    Solution for https://leetcode.com/problems/n-th-tribonacci-number/

---
+++ 1137_Nth_Tribonacci_Number.java
@@ -0,0 +1,7 @@
+public class Solution {
+    public int tribonacci(int n) {
+        if(n<=1) return n;
+        if(n==2) return 1;
+        return tribonacci(n-1)+tribonacci(n-2)+tribonacci(n-3);
+    }
+}
